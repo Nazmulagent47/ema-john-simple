36e594f3f7baa42f5b49037241011412736f0ab7
Nazmul Hasan01~ <nazmulagent47@gmail.com>
2023-03-31 04:48:51 +0600

    grand total added from module51

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({cart}) => {
+    // const cart = props.Cart; // option 1
+    // const {cart} = props; // option 2
+
+    console.log(cart);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for ( const product of cart) {
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+
+    let totalTax = totalPrice * 7 / 100;
+
+    let grandTotal = totalPrice + totalShipping + totalTax;
+
+    return (
+        <div className='cart'>
+            <h2>Order Summary</h2>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Shipping: ${totalShipping}</p>
+            <p>Tax: ${totalTax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+        </div>
+    );
+};
+
+export default Cart;
