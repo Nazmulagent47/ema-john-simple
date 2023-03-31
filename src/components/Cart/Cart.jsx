4448883ab74cc08bca8c8c9fab18ba79d335e46c
Nazmul Hasan01~ <nazmulagent47@gmail.com>
2023-03-31 07:12:04 +0600

    show data in cart from LS

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,23 +5,29 @@ const Cart = ({cart}) => {
     // const cart = props.Cart; // option 1
     // const {cart} = props; // option 2
 
-    console.log(cart);
+    // console.log(cart);
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for ( const product of cart) {
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+        // product.quantity = product.quantity || 1;
+        // if ( product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
 
-    let totalTax = totalPrice*7 / 100;
+    let totalTax = totalPrice * 7 / 100;
 
     let grandTotal = totalPrice + totalShipping + totalTax;
 
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${totalTax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,21 +2,58 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
     const [cart, setCart] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, []);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id of the added product
+        for (const id in storedCart) {
+            // step 2: get the product from products state by using id
+            const addedProduct = products.find(product => product.id === id);
+
+            if (addedProduct) {
+                // step 3: get quantity of the storedCart and add quantity to product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the addedProduct to the saved cart
+                savedCart.push(addedProduct);
+            }
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    }, [products]);
+
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist, then update quantity by 1
+        let newCart = [];
+
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
